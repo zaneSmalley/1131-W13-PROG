bcd2d89d3e5be89db6e72d44418be77173a906b6
Jack Grantham <jackrgrantham@gmail.com>
2019-12-09 21:59:26 -0500

    Started the game class
    Did not accomplish a whole lot. I would suggest you guys to look it over make sure it is what you want at least so far.

--- Week13Program/src/Game.java
+++ Week13Program/src/Game.java
@@ -1,3 +1,5 @@
+import com.sun.javaws.security.AppContextUtil;
+
 /**
  * Last modified on 12/9/2019
  *
@@ -6,5 +8,27 @@
  * CS1131 Fall 2019
  * Lab Section 2
  */
+
+// Keep track of the game state and set up relationship with rooms and items and such
+
 public class Game {
+
+    private Player player;
+    private Room currentRoom;
+    private Room start;
+
+    public Game( Player player) {
+
+        this.player = player;
+        start = new Room("Main Courtyard");
+        start.setRoomDescription( "Main Courtyard..." );
+
+    }
+
+    public void start(Player player) {
+
+
+
+    }
+
 }
